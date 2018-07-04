bfdebf37dbf5db83774696ac944c057f7fe0cfa7
Ed Sweeney <ed@onextent.com>
2018-07-04 08:05:45 -0700

    hiya

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello Prasanta!");
+		System.out.println("Hello from " + System.getenv("RESIN_DEVICE_NAME_AT_INIT"));
 	}
 }
