de92c787b2dfeeefc4128bab8e295effde8b6b0f
Ed Sweeney <ed@onextent.com>
2018-07-04 12:15:38 -0700

    test

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello
 	public static void main(String[] args) throws Exception
 	{
     for (;;) {
-		  System.out.println("Hello from " + System.getenv("RESIN_DEVICE_NAME_AT_INIT"));
+		  System.out.println("Hello from " + System.getenv("RESIN_DEVICE_NAME_AT_INIT") + "!");
       Thread.sleep(5000);
     }
 	}
